fb2a39c24f77f7779dfae7ba910c333d6f35f11a
El-Bromero <brandonaromero@gmail.com>
2021-01-30 13:34:05 -0600

    Added some timer code. Was testing out some code. I can modify timer with current time being displayed. Need to see how to make it show time counting up instead. Have stop and reset button working as intended

--- dom/timer/timer-dom.js
+++ dom/timer/timer-dom.js
@@ -1,2 +1,26 @@
 console.log("time to start the timer!");
 
+const button = document.querySelectorAll("button");
+const p = document.querySelector("p");
+
+button[0].addEventListener('click', () => {
+    let timer = setInterval(() => {
+        let d = new Date();
+        let t = d.toLocaleTimeString();
+        p.textContent = t;
+    }, 1000);
+
+    button[1].addEventListener('click', () => {
+    clearInterval(timer);
+    })
+
+    button[2].addEventListener('click', () => {
+        p.textContent = "0:00";
+        clearInterval(timer);
+    })
+})
+
+
+
+console.log(button);
+console.log(p);
