883d5acf0be4cac2fa66602f7ada0e18ecc50862
El-Bromero <brandonaromero@gmail.com>
2021-01-31 16:50:48 -0600

    Slowly but surely completing inject exercise. Finished appending the first p tag

--- dom/inject/inject.js
+++ dom/inject/inject.js
@@ -22,4 +22,46 @@
 
 */
 
-alert("hello world");
+const div = document.querySelector("div");
+
+const center = document.createElement("center");
+div.appendChild(center);
+
+const h1 = document.createElement("h1");
+const i = document.createElement("i");
+
+center.appendChild(h1);
+h1.appendChild(i);
+i.textContent = "HTML Practice Exercise TEKcamp.";
+
+const h2 = document.createElement("h2");
+const a = document.createElement("a");
+a.setAttribute("href", "#");
+a.textContent = "TEKsystems \"TEKcamp";
+h2.appendChild(a);
+center.appendChild(h2);
+
+const hr = document.createElement("hr");
+div.appendChild(hr);
+
+const p = document.createElement("p");
+p.innerHTML = "I love <i>HTML</i> for the following reasons:";
+div.appendChild(p);
+
+const ol = document.createElement("ol");
+const li = document.createElement("li");
+
+/*
+<hr />
+<p>I love <i>HTML</i> for the following reasons:</p>
+<ol>
+  <li>I learned it quickly.</li>
+  <li>I can make web pages using code
+<li> It’s fun.</li>
+</ol>
+<hr />
+*/
+
+
+console.log(div);
+// console.log(h2);
