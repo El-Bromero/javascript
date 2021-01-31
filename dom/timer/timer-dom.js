eaaebfd3fa8f4c84f1558b9816f2bbfa25a4779f
El-Bromero <brandonaromero@gmail.com>
2021-01-30 22:25:45 -0600

    Implemented correct timer that counts up. Need to make it so that if you click start again after clicking start it does not show multiple timers going up

--- dom/timer/timer-dom.js
+++ dom/timer/timer-dom.js
@@ -4,16 +4,25 @@ const button = document.querySelectorAll("button");
 const p = document.querySelector("p");
 
 button[0].addEventListener('click', () => {
-    let timer = setInterval(() => {
-        let d = new Date();
-        let t = d.toLocaleTimeString();
-        p.textContent = t;
-    }, 1000);
 
+    let sec = 0;
+
+    // Next snippet of code increments the timer value correctly. Took some ideas for correctly getting time from a question in stackoverflow
+    function correctTime(value) {
+        return value > 9 ? value : "0" + value;
+    }
+    let timer = setInterval(() => {
+        secString = correctTime(++sec % 60);
+        minString = correctTime(parseInt(sec / 60, 10));
+        p.textContent = minString + ":" + secString;
+    }, 1000)
+
+    // Stop button uses clearInterval and that method stops timer interval
     button[1].addEventListener('click', () => {
     clearInterval(timer);
     })
 
+    // Reset button resets time by changing text content of p to 0:00
     button[2].addEventListener('click', () => {
         p.textContent = "0:00";
         clearInterval(timer);
